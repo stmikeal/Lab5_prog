c06f5dc8b7ad00fa267e36f37f1ff1ee68aa1bc4
stmikeal <timetocook420@gmail.com>
2021-03-08 02:33:37 +0300

    created command-classes and test 4 env.var

--- window/Console.java
+++ window/Console.java
@@ -1,7 +1,11 @@
 package window;
 
 public class Console {
+    private static final String ENV_INPUT = "LAB_INPUT";
+    private static final String ENV_OUTPUT = "LAB_OUTPUT";
+    
     public static void main(String[] args){
-        
+        System.out.println(System.getenv(ENV_INPUT));
+        System.out.println(System.getenv(ENV_OUTPUT));
     }
 }
